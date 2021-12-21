ab3277592019a83403f2a2da44fb90a87d7c2067
pavanvyn <pavan.vynatheya@gmail.com>
2021-12-21 16:23:25 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ $(window).on("load", function() {
 
     //scroll and click effects
     var aboutMePos = $("#aboutMe").offset().top;
-    var educationPos = $("#education").offset().top;
     var projectsPos = $("#projects").offset().top;
+    var educationPos = $("#education").offset().top;
     var experiencePos = $("#experience").offset().top;
     var activitiesPos = $("#activities").offset().top;
 
@@ -20,11 +20,11 @@ $(window).on("load", function() {
     $(document).on("click", "#aboutMeMenu", function() {
         $("body, html").animate({scrollTop: aboutMePos}, 500, "swing");
     });
-    $(document).on("click", "#educationMenu", function() {
-        $("body, html").animate({scrollTop: educationPos}, 500, "swing");
-    });
     $(document).on("click", "#projectsMenu", function() {
         $("body, html").animate({scrollTop: projectsPos}, 500, "swing");
+    });
+    $(document).on("click", "#educationMenu", function() {
+        $("body, html").animate({scrollTop: educationPos}, 500, "swing");
     });
     $(document).on("click", "#experienceMenu", function() {
         $("body, html").animate({scrollTop: experiencePos}, 500, "swing");
@@ -36,23 +36,23 @@ $(window).on("load", function() {
     var halfHeight = window.innerHeight/2;
     $(window).scroll(function(){
         var windowPos = $(window).scrollTop() + 1;
-        if (windowPos >= aboutMePos - halfHeight && windowPos < educationPos - halfHeight) {
+        if (windowPos >= aboutMePos - halfHeight && windowPos < projectsPos - halfHeight) {
             $("#menubar ul li").children().css("color","#dddddd");
             $("#menubar ul li").children().css("font-weight","normal");
             $("#menubar ul li#aboutMeMenu p").css("color","#ff8c1a");
             $("#menubar ul li#aboutMeMenu p").css("font-weight","bold");
         }
-        else if (windowPos >= educationPos - halfHeight && windowPos < projectsPos - halfHeight) {
-            $("#menubar ul li").children().css("color","#dddddd");
-            $("#menubar ul li").children().css("font-weight","normal");
-            $("#menubar ul li#educationMenu p").css("color","#ff8c1a");
-            $("#menubar ul li#educationMenu p").css("font-weight","bold");
-        }
-        else if (windowPos >= projectsPos - halfHeight && windowPos < experiencePos - halfHeight) {
+        else if (windowPos >= projectsPos - halfHeight && windowPos < educationPos - halfHeight) {
             $("#menubar ul li").children().css("color","#dddddd");
             $("#menubar ul li").children().css("font-weight","normal");
             $("#menubar ul li#projectsMenu p").css("color","#ff8c1a");
             $("#menubar ul li#projectsMenu p").css("font-weight","bold");
+        }
+        else if (windowPos >= educationPos - halfHeight && windowPos < experiencePos - halfHeight) {
+            $("#menubar ul li").children().css("color","#dddddd");
+            $("#menubar ul li").children().css("font-weight","normal");
+            $("#menubar ul li#educationMenu p").css("color","#ff8c1a");
+            $("#menubar ul li#educationMenu p").css("font-weight","bold");
         }
         else if (windowPos >= experiencePos - halfHeight && windowPos < activitiesPos - halfHeight) {
             $("#menubar ul li").children().css("color","#dddddd");
